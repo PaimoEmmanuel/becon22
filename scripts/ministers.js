429a19db1d13164287ba7da249d184879351e0e2
PaimoEmmanuel <paimoemmanuel2016@gmail.com>
2021-07-31 15:59:40 +0100

    ministers dp gen

---
+++ scripts/ministers.js
@@ -0,0 +1,13 @@
+
+//Ministers Button Validation
+document.getElementById("minister_text_1").addEventListener("input", (e) => {
+  console.log(document.getElementById("minister_text_2").value);
+  e.target.value && document.getElementById("minister_text_2").value
+    ? (document.getElementById("minister_btn_upload").disabled = false)
+    : (document.getElementById("minister_btn_upload").disabled = true);
+});
+document.getElementById("minister_text_2").addEventListener("input", (e) => {
+  e.target.value && document.getElementById("minister_text_1").value
+    ? (document.getElementById("minister_btn_upload").disabled = false)
+    : (document.getElementById("minister_btn_upload").disabled = true);
+});
